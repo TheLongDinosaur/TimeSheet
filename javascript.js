52ae71cb2cf349ee505ce266347d5486c4f8f974
hkichen <hkichen@gmail.com>
2018-03-24 12:53:28 -0400

    formating changes to html

--- javascript.js
+++ javascript.js
@@ -1,9 +1,11 @@
 //What happens when Clock In button is clicked
 //0. create new table row, with 3 table data columns (one under name, under clock in , under clock out)
 //1. grab name input and append to table
+    //grab role input and append to table
 //2. grab current time as soon as you click clock in and append it to table
 //3. perhaps have a placeholder value to clockout appear until user clicks clock out
 
 //what happens when clock out is clicked
 //1. grab current time as soon as user clicks clock out and append to table
 
+//Firebase stuff....
