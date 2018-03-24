48b3e43f8585a17fc341878b7044319349e4fa76
hkichen <hkichen@gmail.com>
2018-03-24 14:05:12 -0400

    added JS for updating employee information

--- javascript.js
+++ javascript.js
@@ -1,11 +1,70 @@
-//What happens when Clock In button is clicked
-//0. create new table row, with 3 table data columns (one under name, under clock in , under clock out)
-//1. grab name input and append to table
-    //grab role input and append to table
-//2. grab current time as soon as you click clock in and append it to table
-//3. perhaps have a placeholder value to clockout appear until user clicks clock out
+firebase.initializeApp(config);
 
-//what happens when clock out is clicked
-//1. grab current time as soon as user clicks clock out and append to table
+var database = firebase.database();
 
-//Firebase stuff....
+// 2. Button for adding Employees
+$("#submit").on("click", function(event) {
+  event.preventDefault();
+
+  // Grabs user input
+  var empName = $("#name-input").val().trim();
+  var empRole = $("#role-input").val().trim();
+  var empStart = moment($("#startDate-input").val().trim(), "DD/MM/YY").format("X");
+  var empRate = $("#rate-input").val().trim();
+
+  // Creates local "temporary" object for holding employee data
+  var newEmp = {
+    name: empName,
+    role: empRole,
+    start: empStart,
+    rate: empRate
+  };
+
+  // Uploads employee data to the database
+  database.ref().push(newEmp);
+
+  // Logs everything to console
+  console.log(newEmp.name);
+  console.log(newEmp.role);
+  console.log(newEmp.start);
+  console.log(newEmp.rate);
+
+  // Clears all of the text-boxes
+  $("#name-input").val("");
+  $("#role-input").val("");
+  $("#startDate-input").val("");
+  $("#rate-input").val("");
+});
+
+// 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var empName = childSnapshot.val().name;
+  var empRole = childSnapshot.val().role;
+  var empStart = childSnapshot.val().start;
+  var empRate = childSnapshot.val().rate;
+
+  // Employee Info
+  console.log(empName);
+  console.log(empRole);
+  console.log(empStart);
+  console.log(empRate);
+
+  // make employee start date readable
+  var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
+
+  // To calculate the months worked
+  var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
+  console.log(empMonths);
+
+  // Calculate the total billed rate
+  var empBilled = empMonths * empRate;
+  console.log(empBilled);
+
+  // Add each train's data into the table
+  $("#employee-table > tbody").append("<tr><td>" + empName + "</td><td>" + empRole + "</td><td>" +
+  empStartPretty + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
+});
